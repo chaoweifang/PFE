4e2f334f8f6d9647e5f56835c54be4355be1fafb
chaoweifang <fangcw314@163.com>
2018-01-05 21:49:16 +0800

    clear

--- ExtraImages/Ucm2Seg.m
+++
@@ -1,4 +0,0 @@
-function seg = Ucm2Seg(ucm2,thre)
-labels2 = bwlabel(ucm2 <= thre);
-seg = labels2(2:2:end, 2:2:end);
-end
--- collecttimes.m
+++
@@ -1,7 +0,0 @@
-fils=dir('*.mat');
-t=0;
-for i=1:length(fils)
-    aa=load(fils(i).name);
-    t=t+aa.time;
-end
-t/length(fils)
